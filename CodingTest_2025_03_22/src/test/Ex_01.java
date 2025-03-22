e986d99d6f7ff5c12c0907488ee9f7878b07562d
Sanugie99 <Sanugie99@gmail.com>
2025-03-22 19:46:42 +0900

    2025.03.22

--- CodingTest_2025_03_22/src/test/Ex_01.java
+++ CodingTest_2025_03_22/src/test/Ex_01.java
@@ -1,9 +1,21 @@
 package test;
-
+import java.util.*;
 public class Ex_01 {
-
+	/*
+	 * 문제
+	 * 문자열 str이 주어질 때, str을 출력하는 코드를 작성해 보세요.
+	 * 입력: HelloWorld!
+	 * 출력: HelloWorld!
+	 * 
+	 * 제한사항
+	 * 1 ≤ str의 길이 ≤ 1,000,000
+	 * str에는 공백이 없으며, 첫째 줄에 한 줄로만 주어집니다.
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
 
 	}
 
